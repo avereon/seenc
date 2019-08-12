970fa5fade3a46cbff1426f468b77b91a5ac1a97
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-08-12 10:51:15 -0600

    Continue work to deploy from travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -2,11 +2,10 @@
 
 echo 'avereon.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAX0k5tSvrXVpKl7HNPIPglp6Kyj0Ypty6M3hgR783ViTzhRnojEZvdCXuYiGSVKEzZWr9oYQnLr03qjU/t0SNw=' >> $HOME/.ssh/known_hosts
 
-mkdir .ssh
-openssl aes-256-cbc -K $encrypted_c26b694af5a7_key -iv $encrypted_c26b694af5a7_iv -in .travis/id_rsa.enc -out .ssh/id_rsa -d
-chmod 600 .ssh/id_rsa
-ssh-add .ssh/id_rsa
-ssh-keygen -lf .ssh/id_rsa
+openssl aes-256-cbc -K $encrypted_c26b694af5a7_key -iv $encrypted_c26b694af5a7_iv -in .travis/id_rsa.enc -out /home/travis/.ssh/id_rsa -d
+chmod 600 /home/travis/.ssh/id_rsa
+ssh-add /home/travis/.ssh/id_rsa
+ssh-keygen -lf /home/travis/.ssh/id_rsa
 
 eval "$(ssh-agent -s)"
 scp -v target/product.jar travis@avereon.com:/opt/avn/store/stable/seenc

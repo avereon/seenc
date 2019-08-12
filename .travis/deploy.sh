8c6146946b262e157aae5a09f59f1d66df491d26
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-08-12 11:33:27 -0600

    Continue work to deploy from travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Maven deploy
+mvn --settings .travis/settings.xml -DskipTests=true -Dmaven.javadoc.skip=true -B -U -V deploy
+
+# Avereon deploy
 echo 'avereon.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAX0k5tSvrXVpKl7HNPIPglp6Kyj0Ypty6M3hgR783ViTzhRnojEZvdCXuYiGSVKEzZWr9oYQnLr03qjU/t0SNw=' >> $HOME/.ssh/known_hosts
 openssl aes-256-cbc -K $encrypted_c26b694af5a7_key -iv $encrypted_c26b694af5a7_iv -in .travis/id_rsa.enc -out $HOME/.ssh/id_rsa -d
 chmod 600 $HOME/.ssh/*

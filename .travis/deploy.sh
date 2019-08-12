c498acd3707835d40995ac4c8c6c827ae1e83cc6
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-08-12 10:00:09 -0600

    Continue work to deploy from travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,8 +6,8 @@ mkdir -p /tmp/travis
 openssl aes-256-cbc -K $encrypted_c26b694af5a7_key -iv $encrypted_c26b694af5a7_iv -in .travis/id_rsa.enc -out /tmp/travis/id_rsa -d
 chmod 600 /tmp/travis/id_rsa
 ssh-add /tmp/travis/id_rsa
-eval "$(ssh-agent -s)"
-wc -l /tmp/travis/id_rsa
+ssh-keygen /tmp/travis/id_rsa
 
+#eval "$(ssh-agent -s)"
 scp target/product.jar travis@avereon.com:/opt/avn/store/stable/seenc
 scp target/target/main/java/META-INF/*.card travis@avereon.com:/opt/avn/store/stable/seenc

b57b0c0f87cbbddbf9105de4ca17d542da3372fa
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-08-12 11:30:00 -0600

    Continue work to deploy from travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo 'avereon.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAX0k5tSvrXVpKl7HNPIPglp6Kyj0Ypty6M3hgR783ViTzhRnojEZvdCXuYiGSVKEzZWr9oYQnLr03qjU/t0SNw=' >> $HOME/.ssh/known_hosts
-openssl aes-256-cbc -K $encrypted_c26b694af5a7_key -iv $encrypted_c26b694af5a7_iv -in .travis/id_rsa.enc -out /home/travis/.ssh/id_rsa -d
-chmod 600 /home/travis/.ssh/*
+openssl aes-256-cbc -K $encrypted_c26b694af5a7_key -iv $encrypted_c26b694af5a7_iv -in .travis/id_rsa.enc -out $HOME/.ssh/id_rsa -d
+chmod 600 $HOME/.ssh/*
 
 #eval "$(ssh-agent -s)"
-#ssh-add /home/travis/.ssh/id_rsa
+#ssh-add $HOME/.ssh/id_rsa
 
 scp target/product.jar travis@avereon.com:/opt/avn/store/stable/seenc
-#scp target/target/main/java/META-INF/*.card travis@avereon.com:/opt/avn/store/stable/seenc
+scp target/target/main/java/META-INF/product.card travis@avereon.com:/opt/avn/store/stable/seenc

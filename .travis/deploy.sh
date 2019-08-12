cea71715d088dbaf17a573e391e3f3eecace35c3
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-08-12 11:02:59 -0600

    Continue work to deploy from travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,7 +3,8 @@
 echo 'avereon.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAX0k5tSvrXVpKl7HNPIPglp6Kyj0Ypty6M3hgR783ViTzhRnojEZvdCXuYiGSVKEzZWr9oYQnLr03qjU/t0SNw=' >> $HOME/.ssh/known_hosts
 
 openssl aes-256-cbc -K $encrypted_c26b694af5a7_key -iv $encrypted_c26b694af5a7_iv -in .travis/id_rsa.enc -out /home/travis/.ssh/id_rsa -d
-chmod 600 /home/travis/.ssh/id_rsa
+cp .travis/id_rsa.pub /home/travis/.ssh/id_rsa.pub
+chmod 600 /home/travis/.ssh/*
 ssh-add /home/travis/.ssh/id_rsa
 ssh-keygen -lf /home/travis/.ssh/id_rsa
 

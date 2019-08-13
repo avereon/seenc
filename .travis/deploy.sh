a12b84d2a2cc9e0f72a3e288d0ed3a10fd08b461
Mark Soderquist <mark@soderquist.net>
2019-08-12 18:37:28 -0600

    Enable deployment from travis

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -7,5 +7,5 @@ mvn --settings .travis/settings.xml -DskipTests=true -Dmaven.javadoc.skip=true -
 echo 'avereon.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAX0k5tSvrXVpKl7HNPIPglp6Kyj0Ypty6M3hgR783ViTzhRnojEZvdCXuYiGSVKEzZWr9oYQnLr03qjU/t0SNw=' >> $HOME/.ssh/known_hosts
 openssl aes-256-cbc -K $encrypted_c26b694af5a7_key -iv $encrypted_c26b694af5a7_iv -in .travis/id_rsa.enc -out $HOME/.ssh/id_rsa -d
 chmod 600 $HOME/.ssh/*
-scp -B target/product.jar travis@avereon.com:/opt/avn/store/stable/seenc && echo "Deployed /opt/avn/store/latest/mouse/product.jar"
-scp -B target/main/java/META-INF/product.card travis@avereon.com:/opt/avn/store/stable/seenc && echo "Deployed /opt/avn/store/latest/mouse/product.card"
+scp -B target/product.jar travis@avereon.com:/opt/avn/store/stable/seenc/product.jar && echo "Deployed /opt/avn/store/stable/seenc/product.jar"
+scp -B target/main/java/META-INF/product.card travis@avereon.com:/opt/avn/store/stable/seenc/product.card && echo "Deployed /opt/avn/store/stable/seenc/product.card"

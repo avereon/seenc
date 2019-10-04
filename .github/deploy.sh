07fdab31d49a375058210e06f186b5fca2875906
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-10-04 09:47:24 -0600

    Use gpg files

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -21,18 +21,19 @@ if [ "${PLATFORM}" == "linux" ]; then
   Xvfb ${DISPLAY} -screen 0 1920x1080x24 -nolisten unix &
 fi
 
-rm -rf target/jlink
-mvn deploy -B -U -V -P testui,platform-specific-assemblies --settings .github/settings.xml --file pom.xml
+gpg --quiet --batch --yes --decrypt --passphrase=$AVN_GPG_PASSWORD --output .github/avereon.keystore .github/avereon.keystore.gpg
+rm -rf target/jlink && mvn deploy -B -U -V -P testui,platform-specific-assemblies --settings .github/settings.xml --file pom.xml
 
 echo "Build date=$(date)"
 echo "[github.ref]=${GITHUB_REF}"
 echo "[matrix.os]=${MATRIX_OS}"
 echo "Deploy path=/opt/avn/store/$RELEASE/$PRODUCT"
 
-mkdir ${HOME}/.ssh
-echo "${TRAVIS_SSH_KEY}" > ${HOME}/.ssh/id_rsa
-echo "${TRAVIS_SSH_PUB}" > ${HOME}/.ssh/id_rsa.pub
-echo 'avereon.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAX0k5tSvrXVpKl7HNPIPglp6Kyj0Ypty6M3hgR783ViTzhRnojEZvdCXuYiGSVKEzZWr9oYQnLr03qjU/t0SNw=' >> ${HOME}/.ssh/known_hosts
+mkdir "${HOME}/.ssh"
+gpg --quiet --batch --yes --decrypt --passphrase=$AVN_GPG_PASSWORD --output $HOME/.ssh/id_rsa .github/id_rsa.gpg
+gpg --quiet --batch --yes --decrypt --passphrase=$AVN_GPG_PASSWORD --output $HOME/.ssh/id_rsa.pub .github/id_rsa.pub.gpg
+gpg --quiet --batch --yes --decrypt --passphrase=$AVN_GPG_PASSWORD --output $HOME/.ssh/known_hosts .github/known_hosts.gpg
+
 chmod 600 ${HOME}/.ssh/id_rsa
 chmod 600 ${HOME}/.ssh/id_rsa.pub
 chmod 600 ${HOME}/.ssh/known_hosts

dc1efcfe7b7debdfff8773cfccbaa23a2d62fa58
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-10-04 10:07:15 -0600

    Use gpg files

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -22,6 +22,9 @@ if [ "${PLATFORM}" == "linux" ]; then
 fi
 
 gpg --quiet --batch --yes --decrypt --passphrase=$AVN_GPG_PASSWORD --output .github/avereon.keystore .github/avereon.keystore.gpg
+ls -al .github/avereon.keystore
+sha1sum .github/avereon.keystore
+
 rm -rf target/jlink && mvn deploy -B -U -V -P testui,platform-specific-assemblies --settings .github/settings.xml --file pom.xml
 if [ $? -ne 0 ]; then exit 1; fi
 
@@ -38,6 +41,11 @@ gpg --quiet --batch --yes --decrypt --passphrase=$AVN_GPG_PASSWORD --output $HOM
 chmod 600 ${HOME}/.ssh/id_rsa
 chmod 600 ${HOME}/.ssh/id_rsa.pub
 chmod 600 ${HOME}/.ssh/known_hosts
+
+ls -al "$HOME/.ssh"
+sha1sum "$HOME/.ssh/id_rsa"
+sha1sum "$HOME/.ssh/id_rsa.pub"
+sha1sum "$HOME/.ssh/known_hosts"
 
 scp -B target/*product.jar travis@avereon.com:/opt/avn/store/$RELEASE/$PRODUCT
 if [ $? -ne 0 ]; then exit 1; fi
